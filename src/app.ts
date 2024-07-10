1ab7c0b4f6156202e4407f0344bade6f09ab1360
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2024-07-10 16:07:24 +0800

    Getting user input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
 // Code goes here!
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+form?.addEventListener('submit', (event: Event) => {
+  event.preventDefault();
+
+  const enteredAddress = addressInput.value;
+});
