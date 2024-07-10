60f2df44d2d74a1a78cb87bd477ccbdbf6f17159
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2024-07-10 17:09:47 +0800

    Using Axios to fetch coordinantes for an entered address

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
-// Code goes here!
+import axios from 'axios';
 
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-form?.addEventListener('submit', (event: Event) => {
+type NominatimGeocodingResponse = {
+  features: { geometry: { coordinates: [number, number] } }[];
+};
+
+form?.addEventListener('submit', async (event: Event) => {
   event.preventDefault();
 
   const enteredAddress = addressInput.value;
+
+  try {
+    const respone = await axios.get<NominatimGeocodingResponse>(
+      `https://nominatim.openstreetmap.org/search?q=${encodeURI(
+        enteredAddress
+      )}&format=geojson`
+    );
+  } catch (error) {
+    console.log(error);
+  }
 });
