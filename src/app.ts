10e1f8c1e5f3c96f839d39961f9091dc5f958be9
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2024-07-10 18:15:55 +0800

    Rendering a map with google maps

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,13 @@ const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 type NominatimGeocodingResponse = {
-  features: { geometry: { coordinates: [number, number] } }[];
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
 };
+
+declare var google: any;
+
+const GOOGLE_API_KEY = '';
 
 form?.addEventListener('submit', async (event: Event) => {
   event.preventDefault();
@@ -14,11 +19,24 @@ form?.addEventListener('submit', async (event: Event) => {
 
   try {
     const respone = await axios.get<NominatimGeocodingResponse>(
-      `https://nominatim.openstreetmap.org/search?q=${encodeURI(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
-      )}&format=geojson`
+      )}&key=${GOOGLE_API_KEY}`
     );
-  } catch (error) {
+
+    const coordinantes = respone.data.results[0].geometry.location;
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: coordinantes,
+      zoom: 16,
+    });
+
+    new google.maps.Marker({ position: coordinantes, map });
+
+    if (respone.data.status !== 'OK')
+      throw new Error('Could not fetch location!');
+  } catch (error: any) {
+    alert(error.message);
     console.log(error);
   }
 });
